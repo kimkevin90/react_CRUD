a1c6290d98dba7d21df350c8ae0e87a050abd577
kimkevin90 <kimkevin90@naver.com>
2020-08-20 17:15:22 +0900

    MYSQL APPLY

--- my-react/client/src/App.js
+++ my-react/client/src/App.js
@@ -8,6 +8,7 @@ import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const styles = theme => ({
   root: {
@@ -16,7 +17,10 @@ const styles = theme => ({
     overflowX: 'AUTO'
   },
   table: {
-    minWidth: 1080
+    minWidth: 1080,
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
@@ -24,10 +28,12 @@ const styles = theme => ({
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
@@ -37,6 +43,11 @@ class App extends Component {
     const response = await fetch('/api/customers')
     const body = await response.json()
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed > 100 ? 0 : completed + 1 })
   }
 
   render() {
@@ -94,7 +105,13 @@ class App extends Component {
                   job={c.job}
                 />
               )
-            }) : ""}
+            }) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
