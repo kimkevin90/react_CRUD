897371ffa61edb44fdf0bcca62ef0734608b8ff4
kimkevin90 <kimkevin90@naver.com>
2020-09-24 18:50:35 +0900

    multer

--- my-react/server.js
+++ my-react/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
+const path = require('path');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,7 +22,21 @@ connection.connect();
 
 const multer = require('multer');
 //upload 폴더를 사용자의 파일이 업로드가 되는 공간으로 설정하겠다.
-const upload = multer({ dest: './upload' })
+//const upload = multer({ dest: './upload' })
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, './upload');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
 app.get('/api/customers', (req, res) => {
   connection.query(
     "SELECT * FROM CUSTOMER WHERE isDelete = 0",
@@ -34,11 +49,12 @@ app.get('/api/customers', (req, res) => {
 app.use("/image", express.static("./upload"))
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
+  console.log(req.file, req.body)
   //null을 쓴 이유는 id값은 데이터베이스에 자동으로 등록 되기 때문이다.
   let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, NOW() ,0)';
   //image라는 변수로 실제로 프로필 이미지에 바이너리 데이터를 서버에 전송 하니까
   //그것을 받아올것임.
-  let image = '/image/' + req.file.filename;
+  let image = 'http://localhost:3001/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
   let gender = req.body.gender;
